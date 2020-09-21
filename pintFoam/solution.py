eedcea4cd8196369c23553dc8a64c392fac8dc8e
Johan Hidding <j.hidding@esciencecenter.nl>
2020-09-21 18:48:22 +0200

    test restart from cloned vector

--- pintFoam/solution.py
+++ pintFoam/solution.py
@@ -1,6 +1,8 @@
 # ~\~ language=Python filename=pintFoam/solution.py
 # ~\~ begin <<lit/cylinder.md|pintFoam/solution.py>>[0]
 import subprocess
+import math
+from typing import Optional
 
 from .vector import (BaseCase, Vector, parameter_file)
 
@@ -28,16 +30,19 @@ def get_times(path):
          for s in (path / "adiosData").glob("*.bp")],
         key=float)
 
-def foam(solver: str, dt: float, x: Vector, t_0: float, t_1: float) -> Vector:
+def foam(solver: str, dt: float, x: Vector, t_0: float, t_1: float,
+         write_interval: Optional[int] = None) -> Vector:
     # ~\~ begin <<lit/cylinder.md|pintfoam-solution-function>>[0]
     assert abs(float(x.time) - t_0) < epsilon, f"Times should match: {t_0} != {x.time}."
     y = x.clone()
+    write_interval = write_interval or int(math.ceil((t_1 - t_0) / dt))
     # ~\~ begin <<lit/cylinder.md|set-control-dict>>[0]
     controlDict = parameter_file(y, "system/controlDict")
+    controlDict.content['startFrom'] = "latestTime"
     controlDict.content['startTime'] = t_0
     controlDict.content['endTime'] = t_1
     controlDict.content['deltaT'] = dt
-    controlDict.content['writeInterval'] = 1
+    controlDict.content['writeInterval'] = write_interval
     controlDict.writeFile()
     # ~\~ end
     # ~\~ begin <<lit/cylinder.md|run-solver>>[0]
