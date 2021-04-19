d558c3d1d69454d3b728e4fb9b8ca6d849b1683f
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-04-19 14:54:25 +0200

    Clean up

--- run.sh
+++ run.sh
@@ -2,13 +2,11 @@
 
 # Activate environment
 source ~/miniconda3/etc/profile.d/conda.sh
-conda info --envs
 conda activate cylinder
-conda info --envs
+# Check installation
 python --version
 
 # Activate OpenFOAM
 source /opt/OpenFOAM/setImage_v1906.sh
-
 # Check installation
 blockMesh -help
