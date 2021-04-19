e023a86e4ca737b0dc905eb5a835988ae43d026e
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-04-19 14:48:13 +0200

    Fix bugs

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 
 # Activate environment
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate cylinder
+conda info --envs
+conda activate
 python --version
 
-# Activate
+# Activate OpenFOAM
 source /opt/OpenFOAM/setImage_v1906.sh
 
 # Check installation
